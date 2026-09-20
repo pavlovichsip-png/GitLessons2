654f76a73afc478298f1fa2a17fa906220cfe329
solovey <pavlovich.sip@gmail.com>
2026-09-20 18:57:47 +0300

    коммит для пулл реквест

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Computer computer = new Computer("Asus", 200_000);
+        Computer computer = new Computer("Acer", 200_000);
         OmenHP omenHP = new OmenHP("OmenHP", 240_000);
-        omenHP.setName("Acer", 8_000);
+        omenHP.setName("Honor", 8_000);
         System.out.println(omenHP.memory_size);
         System.out.println(omenHP.name);
         SaveComputer saveComputer = new SaveComputer();
