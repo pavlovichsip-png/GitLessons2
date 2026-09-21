b88306988d9a69eb25a0377dc10bc6f8afec32cf
solovey <pavlovich.sip@gmail.com>
2026-09-21 21:22:08 +0300

    new testiing

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,8 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
+        System.out.println("ckjdfkjdf");
+
+        System.out.println("eze testiiing");
     }
 }
