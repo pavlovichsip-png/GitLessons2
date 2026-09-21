04d3e44813fb4c4f8bb4a995fbb551963979bedb
solovey <pavlovich.sip@gmail.com>
2026-09-21 21:13:50 +0300

    test23

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,7 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
